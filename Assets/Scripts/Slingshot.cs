2d5998b84aea2831833bc5ed7f7fc80d8ba2ce2a
Raaj Thota <43080673+Rajvthota@users.noreply.github.com>
2018-10-05 04:27:17 -0400

    Setting scripts up
    set up starting scripts

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+    public GameObject launchPoint;
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+
+        launchPoint = launchPointTrans.gameObject;
+
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+
+    private void OnMouseExit()
+    {
+        {
+            print("Slingshot:OnMouseExist()");
+        }
+    }
+}
